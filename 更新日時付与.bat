84271671b2bcbb42dd24b4171615994cefb71f07
diamonddust1996 <96047407+diamonddust1996@users.noreply.github.com>
2021-12-13 17:51:57 +0900

    Update and rename 更新日時付与 to 更新日時付与.bat

--- 更新日時付与.bat
+++ 更新日時付与.bat
@@ -1,12 +1,14 @@
 @echo off
 for %%i in (%1) do (
-set tmp=%%~ti
+  set tmp=%%~ti
 )
 set year=%tmp:~0,4%
 set month=%tmp:~5,2%
 set date=%tmp:~8,2%
 set hour=%tmp:~11,2%
 set min=%tmp:~14,2%
-set dt=%year%%month%%date%_%hour%%min%
+set sec=%tep:~17,2%
+set dt=%year%%month%%date%%hour%%min%
+
 cd "%~dp1"
 rename "%~nx1" "%dt%_%~nx1"
