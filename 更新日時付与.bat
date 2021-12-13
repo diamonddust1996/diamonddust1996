f9a8911e75eead5aca32849c2532c25f88b15b82
diamonddust1996 <96047407+diamonddust1996@users.noreply.github.com>
2021-12-13 20:05:32 +0900

    Update 更新日時付与.bat

--- 更新日時付与.bat
+++ 更新日時付与.bat
@@ -12,3 +12,5 @@ set dt=%year%%month%%date%%hour%%min%
 
 cd "%~dp1"
 rename "%~nx1" "%dt%_%~nx1"
+
+https://az764295.vo.msecnd.net/stable/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/VSCode-win32-x64-1.63.0.zip
